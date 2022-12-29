126490f723610e1bbdd916b3d0087e535448250e
kamil <kammmilkos@gmail.com>
2022-12-29 10:46:22 +0100

    try using rgb separate images for detecting lines

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@ using namespace cv;
 int main()
 {
     const int imagesCount = 6;
+    const int colors = 3;
     const int imgPartWidth = 84;
     const int imgPartHeight = 63;
     const int imgPadding = 2;
@@ -30,33 +31,77 @@ int main()
     Mat imageF = fullImage(Range(imgPadding + imgPartHeight, imgPadding + imgPartHeight * 2 - 1), Range(imgPadding * 2 + imgPartWidth * 2, imgPadding * 2 + imgPartWidth * 3 - 1));
     Mat images[imagesCount] = {imageA, imageB, imageC, imageD, imageE, imageF};
     
-    Mat grayImages[imagesCount];
+    Mat separateColorspace[imagesCount][colors];
     for (int i = 0; i < imagesCount; i++) {
-        cvtColor(images[i], grayImages[i], COLOR_BGR2GRAY);
+        Mat colorSplitImage[colors];
+        split(images[i], colorSplitImage);
+        for (int j = 0; j < colors; j++) {
+            Mat imageWithSingleColor[colors];
+            for (int k = 0; k < colors; k++) {
+                if (k == j) {
+                    imageWithSingleColor[k] = colorSplitImage[k].clone();
+                } else {
+                    imageWithSingleColor[k] = Mat::zeros(colorSplitImage[k].rows, colorSplitImage[k].cols, CV_8UC1);
+                }
+            }
+            Mat tmpResult;
+            merge(imageWithSingleColor, colors, tmpResult);
+            separateColorspace[i][j] = tmpResult;
+        }
     }
 
-    Mat gaussianBlurImages[imagesCount];
+    Mat grayImages[imagesCount][colors];
+    for (int i = 0; i < imagesCount; i++) {
+        for (int j = 0; j < colors; j++) {
+            cvtColor(separateColorspace[i][j], grayImages[i][j], COLOR_BGR2GRAY);
+            equalizeHist(grayImages[i][j], grayImages[i][j]);
+        }
+    }
+
+    Mat gaussianBlurImages[imagesCount][colors];
     int kernelSize = 5;
     for (int i = 0; i < imagesCount; i++) {
-        GaussianBlur(grayImages[i], gaussianBlurImages[i], Size(kernelSize, kernelSize), 0);
+        for (int j = 0; j < colors; j++) {
+            GaussianBlur(grayImages[i][j], gaussianBlurImages[i][j], Size(kernelSize, kernelSize), 0);
+        }
     }
 
-    Mat edges[imagesCount];
-    int lowThreshold = 50;
-    int highThreshold = 150;
+    Mat edges[imagesCount][colors];
+    int lowThreshold = 1;
+    int highThreshold = 5;
     int apertureSize = 3;
     bool l2Gradient = false;
     for (int i = 0; i < imagesCount; i++) {
-        Canny(grayImages[i], edges[i], lowThreshold, highThreshold, apertureSize, l2Gradient);
+        for (int j = 0; j < colors; j++) {
+            Canny(grayImages[i][j], edges[i][j], lowThreshold, highThreshold, apertureSize, l2Gradient);
+        }
     }
 
     int rho = 1;
     double theta = M_PI / 180;
     int threshold = 25;
-    int minLineLength = 30;
-    std::vector<Vec4i> lines[imagesCount];
+    int minLineLength = 15;
+    int maxLineGap = 0;
+    std::vector<Vec4i> lines[imagesCount][colors];
     for (int i = 0; i < imagesCount; i++) {
-        HoughLinesP(edges[i], lines[i], rho, theta, threshold, minLineLength);
+        for (int j = 0; j < colors; j++) {
+            HoughLinesP(edges[i][j], lines[i][j], rho, theta, threshold, minLineLength, maxLineGap);
+        }
+    }
+
+    Mat separateColorspaceOutImages[imagesCount][colors];
+    for (int i = 0; i < imagesCount; i++) {
+        for (int j = 0; j < colors; j++) {
+            separateColorspaceOutImages[i][j] = separateColorspace[i][j].clone();
+        }
+    }
+
+    for (int i = 0; i < imagesCount; i++) {
+        for (int j = 0; j < colors; j++) {
+            for(size_t k = 0; k < lines[i][j].size(); k++) {
+                line(separateColorspaceOutImages[i][j], Point(lines[i][j][k][0], lines[i][j][k][1]), Point(lines[i][j][k][2], lines[i][j][k][3]), Scalar(255, 255, 255), 2, LINE_AA);
+            }
+        }
     }
 
     Mat outImages[imagesCount];
@@ -64,9 +109,10 @@ int main()
         outImages[i] = images[i].clone();
     }
 
+    
     for (int i = 0; i < imagesCount; i++) {
-        for(size_t j = 0; j < lines[i].size(); j++) {
-            line(outImages[i], Point(lines[i][j][0], lines[i][j][1]), Point(lines[i][j][2], lines[i][j][3]), Scalar(0, 0, 255), 3, LINE_AA);
+        for (int j = 0; j < colors; j++) {
+            imshow(std::to_string(i) + " " + std::to_string(j), separateColorspaceOutImages[i][j]);
         }
     }
 
